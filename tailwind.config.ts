946905195efefd7968dd576f1314cc3bae609a78
mikisakimura <mikisakimura@gmail.com>
2024-09-10 13:43:00 +0200

    カラー追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        accentColor:"#4270ED",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
