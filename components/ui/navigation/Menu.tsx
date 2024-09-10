c5c46c447eb4167917f8fb9ab487cd2988459c22
mikisakimura <mikisakimura@gmail.com>
2024-09-10 13:46:32 +0200

    navigationメニュー追加

---
+++ components/ui/navigation/Menu.tsx
@@ -0,0 +1,39 @@
+//components/ui/navigation/Menu.tsx
+export const Menu = [
+  {
+    name: 'Messege',
+    href: '/#message',
+  },
+  {
+    name: 'Services',
+    href: '/#services',
+  },
+
+  {
+    name: 'News',
+    href: '/#news',
+  },
+  {
+    name: 'Case study',
+    href: '/#casestudy',
+  },
+  {
+    name: 'Blog',
+    href: '/#blog',
+  },
+//   {
+//     name: 'Plan',
+//     href: '/#plan',
+//   },
+//   {
+//     name: 'Flow',
+//     href: '/#flow',
+//   },
+//   {
+//     name: 'FAQ',
+//     href: '/#faq',
+//   },
+
+]
+
+export default Menu
