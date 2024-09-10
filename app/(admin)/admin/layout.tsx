5cb4b6dcb5aa5a22f9db3ed66cac06c4d0b3891b
mikisakimura <mikisakimura@gmail.com>
2024-09-10 13:52:40 +0200

    notosans設定

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -2,8 +2,17 @@ interface AdminLayoutProps {
   children: React.ReactNode
 }
 
+import { Noto_Sans_JP } from 'next/font/google'
+
+// Noto Sans JP フォントの設定
+const notoSansJP = Noto_Sans_JP({
+  weight: ['400', '500','600', '700', '700','800', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 const AdminMainLayout = ({ children }: AdminLayoutProps) => {
-  return <div>{children}</div>
+  return <div className={`${notoSansJP.className} `}>{children}</div>
 }
 
 export default AdminMainLayout
