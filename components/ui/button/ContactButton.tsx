5195885a14c15ada1bac25000dae9a1a47c61cbb
mikisakimura <mikisakimura@gmail.com>
2024-09-10 13:52:32 +0200

    お問い合わせボタン調整

--- components/ui/button/ContactButton.tsx
+++ components/ui/button/ContactButton.tsx
@@ -8,7 +8,7 @@ const ContactButton = () => {
       href="/contact"
       className="bg-accentColor text-white cursor-pointer flex items-center w-full h-20 px-10 font-semibold"
     >
-      <EnvelopeIcon className="w-4 h-4 ml-1" />
+      <EnvelopeIcon className="w-4 h-4 mr-1" />
       お問い合わせ
     </Link>
   )
