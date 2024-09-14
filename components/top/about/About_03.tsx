b5c572977977a0a600a30ae8657b89b9e4e1333c
Tsukimori <36286785+Tsukimori@users.noreply.github.com>
2024-09-14 17:01:49 +0900

    About追加

---
+++ components/top/about/About_03.tsx
@@ -0,0 +1,46 @@
+// components/message/About_03.tsx
+
+"use client"
+
+import MoreLinkButton from "@/components/ui/button/MoreButton"
+import ContentHeadline from "@/components/ui/frame/ContentHeadline"
+import PageContent from "@/components/ui/frame/PageContent"
+import Image from "next/image"
+
+// メッセージ
+const About_03 = () => {
+  return (
+    <>
+      <PageContent>
+        {/* widthがフルサイズでない場合は指定する */}
+        <section className=" w-1200 mx-auto ">
+          <ContentHeadline entitle="Who we are" maintitle="私たちについて" />
+          <div className=" mt-16 flex justify-start space-x-20">
+            <div className="w-[600px]">
+              <h3 className="text-5xl ">見出しテキスト</h3>
+              <p>
+                メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。
+              </p>
+              <MoreLinkButton href="/" />
+            </div>
+            <div className=" w-[520px] h-[767px] relative">
+              <Image
+                src="/top/message/message_img.png"
+                alt="message"
+                layout="fill" // 画像のサイズは親要素にフィット
+                objectFit="cover" // 画像を親要素に合わせてトリミング
+                className="block"
+              />
+            </div>
+          </div>
+        </section>
+      </PageContent>
+    </>
+  )
+}
+
+export default About_03
---
+++ components/top/about/About_04.tsx
@@ -0,0 +1,37 @@
+// components/message/About_04.tsx
+
+"use client"
+
+import MoreLinkButton from "@/components/ui/button/MoreButton"
+import ContentHeadline from "@/components/ui/frame/ContentHeadline"
+import PageContent from "@/components/ui/frame/PageContent"
+import Image from "next/image"
+
+// メッセージ
+const About_04 = () => {
+  return (
+    <>
+      <PageContent>
+        {/* widthがフルサイズでない場合は指定する */}
+        <section className=" w-1200 mx-auto ">
+          <div className=" flex justify-start">
+            <ContentHeadline entitle="Who we are" maintitle="私たちについて" />
+            <div>
+              <h3 className=" w-[500px] text-5xl mt-4 ">見出しテキスト</h3>
+              <p className=" w-[660px]">
+                メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。メッセージはいります。
+                メッセージはいります。メッセージはいります。
+              </p>
+              <MoreLinkButton href="/" />
+            </div>
+          </div>
+        </section>
+      </PageContent>
+    </>
+  )
+}
+
+export default About_04
