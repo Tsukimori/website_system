1089d87c64a48cfbddce124ac9ef21f00edb5608
mikisakimura <mikisakimura@gmail.com>
2024-10-29 16:56:55 +0100

    会社概要のデザイン適応

--- components/top/profile/Profile_02.tsx
+++ components/top/profile/Profile_02.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import Link from "next/link"
 import ContentHeadline from "@/components/ui/frame/ContentHeadline"
 import PageContent from "@/components/ui/frame/PageContent"
+import ProfileCard from "@/components/ui/ItemCard/ProfileCard"
 
 const Profile_02 = () => {
   return (
@@ -18,52 +19,28 @@ const Profile_02 = () => {
             entitleClassName=""
             titleClassName=""
           />
-          <div className="mt-16 bg-white flex">
+          <div className="mt-16 bg-white p-10 flex justify-between">
             <div>
-              <dl className="flex">
-                <dt>社名</dt>
-                <dd>企業名</dd>
-              </dl>
-              <dl className="flex">
-                <dt>設立</dt>
-                <dd>0000年00月00日</dd>
-              </dl>
-              <dl className="flex">
-                <dt>資本金</dt>
-                <dd>000円</dd>
-              </dl>
-              <dl className="flex">
-                <dt>所在地</dt>
-                <dd>000-000○○県○○市○○町00-00○○○○○ビル00F</dd>
-              </dl>
-              <dl className="flex">
-                <dt>電話番号・FAX</dt>
-                <dd>電話番号　000-000-0000／FAX　000-000-0000</dd>
-              </dl>
-              <dl className="flex">
-                <dt>FAX</dt>
-                <dd>000-000-0000</dd>
-              </dl>
-              <dl className="flex">
-                <dt>代表者</dt>
-                <dd>氏名　氏名</dd>
-              </dl>
-              <dl className="flex">
-                <dt>URL</dt>
-                <dd>example@aa.aaaa</dd>
-              </dl>
+            <ProfileCard label="社名" value="企業名" />
+              <ProfileCard label="設立" value="0000年00月00日" />
+              <ProfileCard label="資本金" value="000円" />
+              <ProfileCard label="所在地" value={`000-000\n○○県○○市○○町00-00\n○○○○○ビル00F`} />
+              <ProfileCard label="電話番号" value="000-000-0000" />
+              <ProfileCard label="FAX" value="000-000-0000" />
+              <ProfileCard label="代表者" value="氏名　氏名" />
+              <ProfileCard label="URL" value="example@aa.aaaa" />
             </div>
-            <div>
+            <div className="">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.827853707453!2d139.76454987585436!3d35.68124052997326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sfr!4v1728031590235!5m2!1sja!2sfr"
-                width="1200"
-                height="400"
+                width="420"
+                height="523"
                 style={{ border: "0" }}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               />
 
-              <p>
+              <p className="mt-4 font-light">
                 000-000
                 <br />
                 ○○県○○市○○町00-00○○○○○ビル00F
--- components/top/profile/profile_03.tsx
+++ components/top/profile/profile_03.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import Link from "next/link"
 import ContentHeadline from "@/components/ui/frame/ContentHeadline"
 import PageContent from "@/components/ui/frame/PageContent"
+import ProfileCard from "@/components/ui/ItemCard/ProfileCard"
 
 const Profile_03 = () => {
   return (
@@ -20,93 +21,22 @@ const Profile_03 = () => {
           />
           <div className="mt-16">
             <div>
-              <dl className="flex">
-                <dt>社名</dt>
-                <dd>企業名</dd>
-              </dl>
-              <dl className="flex">
-                <dt>設立</dt>
-                <dd>0000年00月00日</dd>
-              </dl>
-              <dl className="flex">
-                <dt>資本金</dt>
-                <dd>000円</dd>
-              </dl>
-              <dl className="flex">
-                <dt>所在地</dt>
-                <dd>
-                  000-000
-                  <br />
-                  ○○県○○市○○町00-00
-                  <br />
-                  ○○○○○ビル00F
-                </dd>
-              </dl>
-              <dl className="flex">
-                <dt>電話番号</dt>
-                <dd>000-000-0000</dd>
-              </dl>
-              <dl className="flex">
-                <dt>FAX</dt>
-                <dd>000-000-0000</dd>
-              </dl>
-              <dl className="flex">
-                <dt>代表者</dt>
-                <dd>氏名　氏名</dd>
-              </dl>
-              <dl className="flex">
-                <dt>URL</dt>
-                <dd>example@aa.aaaa</dd>
-              </dl>
-            </div>
-            <div>
-              <dl className="flex">
-                <dt>役員</dt>
-                <dd>
-                  役職　○○○○○○ 役職　○○○○○○ 役職　○○○○○○ 役職
-                  <br />
-                  　○○○○○○
-                  <br />
-                  役職　○○○○○○
-                </dd>
-              </dl>
-              <dl className="flex">
-                <dt>従業員</dt>
-                <dd>0000名</dd>
-              </dl>
-              <dl className="flex">
-                <dt>事業内容</dt>
-                <dd>
-                  ○○○○○○○○○○
-                  <br />
-                  ○○○○○○○○○
-                  <br />
-                  ○○○○○○
-                </dd>
-              </dl>
-              <dl className="flex">
-                <dt>主要取引先</dt>
-                <dd>
-                  ○○○○○○○○○○
-                  <br />
-                  ○○○○○○○○○
-                  <br />
-                  ○○○○○○
-                </dd>
-              </dl>
-              <dl className="flex">
-                <dt>主要取引銀行</dt>
-                <dd>
-                  ○○○○○○○○○○
-                  <br />
-                  ○○○○○○○○○
-                  <br />
-                  ○○○○○○
-                </dd>
-              </dl>
+            <ProfileCard label="社名" value="企業名" />
+              <ProfileCard label="設立" value="0000年00月00日" />
+              <ProfileCard label="資本金" value="000円" />
+              <ProfileCard label="所在地" value={`000-000\n○○県○○市○○町00-00\n○○○○○ビル00F`} />
+              <ProfileCard label="電話番号" value="000-000-0000" />
+              <ProfileCard label="FAX" value="000-000-0000" />
+              <ProfileCard label="代表者" value="氏名　氏名" />
+              <ProfileCard label="URL" value="example@aa.aaaa" />
+              <ProfileCard label="役員" value={`役職　○○○○○○\n役職　○○○○○○\n役職　○○○○○○`} />
+              <ProfileCard label="従業員" value="0000名" />
+              <ProfileCard label="事業内容" value={`○○○○○○○○○○\n○○○○○○○○○`} />
+              <ProfileCard label="主要取引先" value={`○○○○○○○○○○\n○○○○○○○○○`} />
+              <ProfileCard label="主要取引銀行" value={`○○○○○○○○○○\n○○○○○○○○○`} />
             </div>
           </div>
-          <div>
+          <div className="mt-20">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.827853707453!2d139.76454987585436!3d35.68124052997326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sfr!4v1728031590235!5m2!1sja!2sfr"
               width="1200"
@@ -116,7 +46,7 @@ const Profile_03 = () => {
               referrerPolicy="no-referrer-when-downgrade" 
             />
 
-            <p>
+            <p className="mt-4 font-light">
               000-000
               <br />
               ○○県○○市○○町00-00○○○○○ビル00F
