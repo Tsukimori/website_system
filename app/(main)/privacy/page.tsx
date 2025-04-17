11f895e8851b145f71f8e46c6cb1c51cd702f3a4
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-04-17 16:00:03 +0900

    privacyページ

---
+++ app/(main)/privacy/page.tsx
@@ -0,0 +1,13 @@
+import Privacy1 from "@/components/privacy/Privacy_01"
+
+// 私たちについて
+const PrivacyPage = () => {
+  return (
+    <div>
+      <Privacy1 />
+   
+    </div>
+  )
+}
+
+export default PrivacyPage
--- lib/componentsConfig.tsx
+++ lib/componentsConfig.tsx
@@ -1,89 +1,105 @@
-import Header_01 from "@/components/header/Header_01"
-import Header_02 from "@/components/header/Header_02"
-import Header_03 from "@/components/header/Header_03"
-import Header_04 from "@/components/header/Header_04"
-import About1 from "@/components/about/About1"
-import About2 from "@/components/about/About2"
-import Footer1 from "@/components/footer/Footer1"
-import Footer2 from "@/components/footer/Footer2"
-import KeyVisual_01 from "@/components/top/kv/KeyVisual_01"
-import KeyVisual_02 from "@/components/top/kv/KeyVisual_02"
-import KeyVisual_03 from "@/components/top/kv/KeyVisual_03"
-import KeyVisual_04 from "@/components/top/kv/KeyVisual_04"
-import KeyVisual_05 from "@/components/top/kv/KeyVisual_05"
-import KeyVisual_06 from "@/components/top/kv/KeyVisual_06"
-import About_01 from "@/components/top/about/About_01"
-import About_02 from "@/components/top/about/About_02"
-import About_03 from "@/components/top/about/About_03"
-import About_04 from "@/components/top/about/About_04"
-import Strength_01 from "@/components/top/strength/Strength_01"
-import Strength_02 from "@/components/top/strength/Strength_02"
-import Strength_03 from "@/components/top/strength/Strength_03"
-import Strength_04 from "@/components/top/strength/Strength_04"
-import Message_01 from "@/components/top/message/Message_01"
-import Message_02 from "@/components/top/message/Message_02"
-import Message_03 from "@/components/top/message/Message_03"
-import Message_04 from "@/components/top/message/Message_04"
-import Service_01 from "@/components/top/service/Service_01"
-import Service_02 from "@/components/top/service/Service_02"
-import Service_03 from "@/components/top/service/Service_03"
-import Service_04 from "@/components/top/service/Service_04"
-import News_01 from "@/components/top/news/News_01"
-import News_02 from "@/components/top/news/News_02"
-import News_03 from "@/components/top/news/News_03"
-import News_04 from "@/components/top/news/News_04"
-import News_05 from "@/components/top/news/News_05"
-import News_06 from "@/components/top/news/News_06"
-import Case_01 from "@/components/top/case/Case_01"
-import Case_02 from "@/components/top/case/Case_02"
-import Case_03 from "@/components/top/case/Case_03"
-import Case_04 from "@/components/top/case/Case_04"
-import Case_05 from "@/components/top/case/Case_05"
-import Case_06 from "@/components/top/case/Case_06"
-import Blog_01 from "@/components/top/blog/Blog_01"
-import Blog_02 from "@/components/top/blog/Blog_02"
-import Blog_03 from "@/components/top/blog/Blog_03"
-import Blog_04 from "@/components/top/blog/Blog_04"
-import Blog_05 from "@/components/top/blog/Blog_05"
-import Blog_06 from "@/components/top/blog/Blog_06"
-import Plan_01 from "@/components/top/plan/Plan_01"
-import Plan_02 from "@/components/top/plan/Plan_02"
-import Plan_03 from "@/components/top/plan/Plan_03"
-import Plan_04 from "@/components/top/plan/Plan_04"
-import Faq_01 from "@/components/top/faq/Faq_01"
-import Faq_02 from "@/components/top/faq/Faq_02"
-import Profile_01 from "@/components/top/profile/Profile_01"
-import Profile_02 from "@/components/top/profile/Profile_02"
-import Profile_03 from "@/components/top/profile/profile_03"
-import Profile_04 from "@/components/top/profile/profile_04"
-import Flow_01 from "@/components/top/flow/Flow_01"
-import Flow_02 from "@/components/top/flow/Flow_02"
-import Cta_01 from "@/components/ui/module/cta/Cta_01"
-import Cta_02 from "@/components/ui/module/cta/Cta_02"
-import Cta_03 from "@/components/ui/module/cta/Cta_03"
-import Cta_04 from "@/components/ui/module/cta/Cta_04"
-import Aside_01 from "@/components/ui/module/aside/Aside_01"
-import Aside_02 from "@/components/ui/module/aside/Aside_02"
+import Header_01 from "@/components/header/Header_01";
+import Header_02 from "@/components/header/Header_02";
+import Header_03 from "@/components/header/Header_03";
+import Header_04 from "@/components/header/Header_04";
+// import About1 from "@/components/about/About1";
+// import About2 from "@/components/about/About_02";
+import Footer1 from "@/components/footer/Footer1";
+import Footer2 from "@/components/footer/Footer2";
+import KeyVisual_01 from "@/components/top/kv/KeyVisual_01";
+import KeyVisual_02 from "@/components/top/kv/KeyVisual_02";
+import KeyVisual_03 from "@/components/top/kv/KeyVisual_03";
+import KeyVisual_04 from "@/components/top/kv/KeyVisual_04";
+import KeyVisual_05 from "@/components/top/kv/KeyVisual_05";
+import KeyVisual_06 from "@/components/top/kv/KeyVisual_06";
+import About_01 from "@/components/top/about/About_01";
+import About_02 from "@/components/top/about/About_02";
+import About_03 from "@/components/top/about/About_03";
+import About_04 from "@/components/top/about/About_04";
+import Strength_01 from "@/components/top/strength/Strength_01";
+import Strength_02 from "@/components/top/strength/Strength_02";
+import Strength_03 from "@/components/top/strength/Strength_03";
+import Strength_04 from "@/components/top/strength/Strength_04";
+import Message_01 from "@/components/top/message/Message_01";
+import Message_02 from "@/components/top/message/Message_02";
+import Message_03 from "@/components/top/message/Message_03";
+import Message_04 from "@/components/top/message/Message_04";
+import Service_01 from "@/components/top/service/Service_01";
+import Service_02 from "@/components/top/service/Service_02";
+import Service_03 from "@/components/top/service/Service_03";
+import Service_04 from "@/components/top/service/Service_04";
+import News_01 from "@/components/top/news/News_01";
+import News_02 from "@/components/top/news/News_02";
+import News_03 from "@/components/top/news/News_03";
+import News_04 from "@/components/top/news/News_04";
+import News_05 from "@/components/top/news/News_05";
+import News_06 from "@/components/top/news/News_06";
+import Case_01 from "@/components/top/case/Case_01";
+import Case_02 from "@/components/top/case/Case_02";
+import Case_03 from "@/components/top/case/Case_03";
+import Case_04 from "@/components/top/case/Case_04";
+import Case_05 from "@/components/top/case/Case_05";
+import Case_06 from "@/components/top/case/Case_06";
+import Blog_01 from "@/components/top/blog/Blog_01";
+import Blog_02 from "@/components/top/blog/Blog_02";
+import Blog_03 from "@/components/top/blog/Blog_03";
+import Blog_04 from "@/components/top/blog/Blog_04";
+import Blog_05 from "@/components/top/blog/Blog_05";
+import Blog_06 from "@/components/top/blog/Blog_06";
+import Plan_01 from "@/components/top/plan/Plan_01";
+import Plan_02 from "@/components/top/plan/Plan_02";
+import Plan_03 from "@/components/top/plan/Plan_03";
+import Plan_04 from "@/components/top/plan/Plan_04";
+import Faq_01 from "@/components/top/faq/Faq_01";
+import Faq_02 from "@/components/top/faq/Faq_02";
+import Profile_01 from "@/components/top/profile/Profile_01";
+import Profile_02 from "@/components/top/profile/Profile_02";
+import Profile_03 from "@/components/top/profile/profile_03";
+import Profile_04 from "@/components/top/profile/profile_04";
+import Flow_01 from "@/components/top/flow/Flow_01";
+import Flow_02 from "@/components/top/flow/Flow_02";
+import Cta_01 from "@/components/ui/module/cta/Cta_01";
+import Cta_02 from "@/components/ui/module/cta/Cta_02";
+import Cta_03 from "@/components/ui/module/cta/Cta_03";
+import Cta_04 from "@/components/ui/module/cta/Cta_04";
+import Aside_01 from "@/components/ui/module/aside/Aside_01";
+import Aside_02 from "@/components/ui/module/aside/Aside_02";
+import Lowerkv_01 from "@/components/lowerkv/Lowerkv_01";
+import Lowerkv_02 from "@/components/lowerkv/Lowerkv_02";
+import Lowerkv_03 from "@/components/lowerkv/Lowerkv_03";
+import Lowerkv_04 from "@/components/lowerkv/Lowerkv_04";
+import Privacy_01 from "@/components/privacy/Privacy_01";
+import Site_01 from "@/components/site/Site_01";
+import LowerAbout_01 from "@/components/about/about/About_01";
+import LowerAbout_02 from "@/components/about/about/About_02";
+import LowerAbout_03 from "@/components/about/about/About_03";
+import LowerAbout_04 from "@/components/about/about/About_04";
+import Purpose_01 from "@/components/about/purpose/Purpose_01";
+import History_01 from "@/components/about/history/History_01";
+import LowerMessage_01 from "@/components/about/message/Message_01";
+import LowerMessage_02 from "@/components/about/message/Message_02";
+import LowerMessage_03 from "@/components/about/message/Message_03";
+import LowerMessage_04 from "@/components/about/message/Message_04";
 
 // セクション情報とコンポーネントのマッピングの型定義
 interface SectionOption {
-  id: string
-  src: string
-  alt: string
+  id: string;
+  src: string;
+  alt: string;
 }
 
 interface SectionInfo {
-  title: string
-  options: SectionOption[]
-  components: { [key: string]: JSX.Element }
+  title: string;
+  options: SectionOption[];
+  components: { [key: string]: JSX.Element };
 }
 
 interface PageConfig {
-  sections: { [key: string]: SectionInfo }
+  sections: { [key: string]: SectionInfo };
 }
 
 interface ComponentsConfig {
-  [key: string]: PageConfig
+  [key: string]: PageConfig;
 }
 
 // ページ情報とコンポーネントのマッピング
@@ -139,7 +155,7 @@ export const componentsConfig: ComponentsConfig = {
           About_04: <About_04 />,
         },
       },
-      
+
       // message: {
       //   title: "メッセージ",
       //   options: [
@@ -247,7 +263,6 @@ export const componentsConfig: ComponentsConfig = {
         components: {
           Flow_01: <Flow_01 />,
           Flow_02: <Flow_02 />,
-
         },
       },
       news: {
@@ -278,11 +293,9 @@ export const componentsConfig: ComponentsConfig = {
         components: {
           Faq_01: <Faq_01 />,
           Faq_02: <Faq_02 />,
-
         },
       },
-      
-      
+
       aside: {
         title: "ASIDE",
         options: [
@@ -307,7 +320,6 @@ export const componentsConfig: ComponentsConfig = {
           Profile_02: <Profile_02 />,
           Profile_03: <Profile_03 />,
           Profile_04: <Profile_04 />,
-
         },
       },
       cta: {
@@ -355,15 +367,187 @@ export const componentsConfig: ComponentsConfig = {
           Header_04: <Header_04 />,
         },
       },
-      about: {
-        title: "メッセージ",
+      lowerkv: {
+        title: "下層キービジュアル",
         options: [
-          { id: "About1", src: "/admin/1.png", alt: "About1" },
-          { id: "About2", src: "/admin/2.png", alt: "About2" },
+          { id: "Lowerkv_01", src: "/admin/1.png", alt: "Lowerkv_01" },
+          { id: "Lowerkv_02", src: "/admin/2.png", alt: "Lowerkv_02" },
+          { id: "Lowerkv_03", src: "/admin/3.png", alt: "Lowerkv_03" },
+          { id: "Lowerkv_04", src: "/admin/4.png", alt: "Lowerkv_04" },
         ],
         components: {
-          About1: <About1 />,
-          About2: <About2 />,
+          Lowerkv_01: <Lowerkv_01 />,
+          Lowerkv_02: <Lowerkv_02 />,
+          Lowerkv_03: <Lowerkv_03 />,
+          Lowerkv_04: <Lowerkv_04 />,
+        },
+      },
+      about: {
+        title: "私たちについて",
+        options: [
+          { id: "LowerAbout_01", src: "/admin/1.png", alt: "LowerAbout_01" },
+          { id: "LowerAbout_02", src: "/admin/2.png", alt: "LowerAbout_02" },
+          { id: "LowerAbout_03", src: "/admin/3.png", alt: "LowerAbout_03" },
+          { id: "LowerAbout_04", src: "/admin/4.png", alt: "LowerAbout_04" },
+        ],
+        components: {
+          LowerAbout_01: <LowerAbout_01 />,
+          LowerAbout_02: <LowerAbout_02 />,
+          LowerAbout_03: <LowerAbout_03 />,
+          LowerAbout_04: <LowerAbout_04 />,
+        },
+      },
+      purpose: {
+        title: "私たちの想い",
+        options: [
+          { id: "Purpose_01", src: "/admin/1.png", alt: "Purpose_01" },
+        ],
+        components: {
+          Purpose_01: <Purpose_01 />,
+        },
+      },
+      history: {
+        title: "企業沿革",
+        options: [
+          { id: "History_01", src: "/admin/1.png", alt: "History_01" },
+        ],
+        components: {
+          History_01: <History_01 />,
+
+        },
+      },
+      message: {
+        title: "メッセージ",
+        options: [
+          { id: "LowerMessage_01", src: "/admin/1.png", alt: "LowerMessage_01" },
+          { id: "LowerMessage_02", src: "/admin/2.png", alt: "LowerMessage_02" },
+          { id: "LowerMessage_03", src: "/admin/3.png", alt: "LowerMessage_03" },
+          { id: "LowerMessage_04", src: "/admin/4.png", alt: "LowerMessage_04" },
+        ],
+        components: {
+          LowerMessage_01: <LowerMessage_01 />,
+          LowerMessage_02: <LowerMessage_02 />,
+          LowerMessage_03: <LowerMessage_03 />,
+          LowerMessage_04: <LowerMessage_04 />,
+        },
+      },
+      profile: {
+        title: "会社概要",
+        options: [
+          { id: "Profile_01", src: "/admin/1.png", alt: "Profile_01" },
+          { id: "Profile_02", src: "/admin/2.png", alt: "Profile_02" },
+          { id: "Profile_03", src: "/admin/3.png", alt: "Profile_03" },
+          { id: "Profile_04", src: "/admin/4.png", alt: "Profile_04" },
+        ],
+        components: {
+          Profile_01: <Profile_01 />,
+          Profile_02: <Profile_02 />,
+          Profile_03: <Profile_03 />,
+          Profile_04: <Profile_04 />,
+        },
+      },
+      footer: {
+        title: "フッター",
+        options: [
+          { id: "Footer1", src: "/admin/1.png", alt: "Footer1" },
+          { id: "Footer2", src: "/admin/2.png", alt: "Footer2" },
+        ],
+        components: {
+          Footer1: <Footer1 />,
+          Footer2: <Footer2 />,
+        },
+      },
+    },
+  },
+  Privacy: {
+    sections: {
+      header: {
+        title: "ヘッダー",
+        options: [
+          { id: "Header_01", src: "/admin/1.png", alt: "Header_01" },
+          { id: "Header_02", src: "/admin/2.png", alt: "Header_02" },
+          { id: "Header_03", src: "/admin/3.png", alt: "Header_03" },
+          { id: "Header_04", src: "/admin/4.png", alt: "Header_04" },
+        ],
+        components: {
+          Header_01: <Header_01 />,
+          Header_02: <Header_02 />,
+          Header_03: <Header_03 />,
+          Header_04: <Header_04 />,
+        },
+      },
+      lowerkv: {
+        title: "下層キービジュアル",
+        options: [
+          { id: "Lowerkv_01", src: "/admin/1.png", alt: "Lowerkv_01" },
+          { id: "Lowerkv_02", src: "/admin/2.png", alt: "Lowerkv_02" },
+          { id: "Lowerkv_03", src: "/admin/3.png", alt: "Lowerkv_03" },
+          { id: "Lowerkv_04", src: "/admin/4.png", alt: "Lowerkv_04" },
+        ],
+        components: {
+          Lowerkv_01: <Lowerkv_01 />,
+          Lowerkv_02: <Lowerkv_02 />,
+          Lowerkv_03: <Lowerkv_03 />,
+          Lowerkv_04: <Lowerkv_04 />,
+        },
+      },
+      privacy: {
+        title: "プライバシーポリシー",
+        options: [{ id: "Privacy_01", src: "/admin/1.png", alt: "Privacy_01" }],
+        components: {
+          Privacy_01: <Privacy_01 />,
+        },
+      },
+      footer: {
+        title: "フッター",
+        options: [
+          { id: "Footer1", src: "/admin/1.png", alt: "Footer1" },
+          { id: "Footer2", src: "/admin/2.png", alt: "Footer2" },
+        ],
+        components: {
+          Footer1: <Footer1 />,
+          Footer2: <Footer2 />,
+        },
+      },
+    },
+  },
+  Site: {
+    sections: {
+      header: {
+        title: "ヘッダー",
+        options: [
+          { id: "Header_01", src: "/admin/1.png", alt: "Header_01" },
+          { id: "Header_02", src: "/admin/2.png", alt: "Header_02" },
+          { id: "Header_03", src: "/admin/3.png", alt: "Header_03" },
+          { id: "Header_04", src: "/admin/4.png", alt: "Header_04" },
+        ],
+        components: {
+          Header_01: <Header_01 />,
+          Header_02: <Header_02 />,
+          Header_03: <Header_03 />,
+          Header_04: <Header_04 />,
+        },
+      },
+      lowerkv: {
+        title: "下層キービジュアル",
+        options: [
+          { id: "Lowerkv_01", src: "/admin/1.png", alt: "Lowerkv_01" },
+          { id: "Lowerkv_02", src: "/admin/2.png", alt: "Lowerkv_02" },
+          { id: "Lowerkv_03", src: "/admin/3.png", alt: "Lowerkv_03" },
+          { id: "Lowerkv_04", src: "/admin/4.png", alt: "Lowerkv_04" },
+        ],
+        components: {
+          Lowerkv_01: <Lowerkv_01 />,
+          Lowerkv_02: <Lowerkv_02 />,
+          Lowerkv_03: <Lowerkv_03 />,
+          Lowerkv_04: <Lowerkv_04 />,
+        },
+      },
+      privacy: {
+        title: "サイトポリシー",
+        options: [{ id: "Site_01", src: "/admin/1.png", alt: "Site_01" }],
+        components: {
+          Site_01: <Site_01 />,
         },
       },
       footer: {
@@ -380,4 +564,4 @@ export const componentsConfig: ComponentsConfig = {
     },
   },
   // ここに新しいページを追加していく
-}
+};
