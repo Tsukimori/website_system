0b8de293e612803c5ec9a80fe416b67ec1d591ce
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-04-20 12:55:26 +0900

    serviceページ調整

---
+++ components/service/service3/Service_03.tsx
@@ -0,0 +1,38 @@
+// components/message/Service_03.tsx
+
+"use client";
+
+import MoreLinkButton from "@/components/ui/button/MoreButton";
+import ContentHeadline from "@/components/ui/frame/ContentHeadline";
+import SectionContent from "@/components/ui/frame/SectionContent";
+import Image from "next/image";
+import ServiceSection_03 from "@/components/ui/module/ServiceSection_03";
+
+// メッセージ
+const Service_03 = () => {
+  return (
+    <>
+      <SectionContent className="![padding-top:0]">
+        {/* widthがフルサイズでない場合は指定する */}
+        <section className="w-full md:max-w-[1200px] mx-auto">
+          {/* <ContentHeadline
+            enTitle="Feature 01"
+            mainTitle="サービスの特徴1"
+            enTitleClassName="text-center"
+            titleClassName="text-center"
+          /> */}
+          <ServiceSection_03
+            enTitle="Feature 01"
+            jaTitle="サービスの特徴1"
+            headingText="見出しテキストはいります見出しテキストはいります見出しテキストはいります見出しテキストはいります見出しテキストはいります"
+            description={`テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。す。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。テキストはいります。`}
+            buttonHref="/about"
+            // imageUrl="/path/to/image.jpg"
+          />
+        </section>
+      </SectionContent>
+    </>
+  );
+};
+
+export default Service_03;
---
+++ components/ui/module/ServiceSection_01.tsx
@@ -0,0 +1,65 @@
+import MoreButton from "@/components/ui/button/MoreButton";
+import Image from "next/image";
+
+interface ServiceSection_01Props {
+  enTitle: string; // タイトル（英語）
+  jaTitle: string; // タイトル（日本語）
+  headingText: string; // 見出しテキスト
+  description: string; // 本文
+  buttonHref?: string; // ボタンリンク先（任意）
+  imageUrl?: string; // 画像URL（任意）
+}
+
+const ServiceSection_01 = ({
+  enTitle,
+  jaTitle,
+  headingText,
+  description,
+  buttonHref,
+  imageUrl,
+}: ServiceSection_01Props) => {
+  const convertNewLines = (text: string) => {
+    return text.split("\\n").map((line, i) => (
+      <span key={i}>
+        {line}
+        {i !== text.split("\\n").length - 1 && <br />}
+      </span>
+    ));
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row justify-start gap-10 md:gap-20">
+      <div className="md:w-1/2 relative min-h-[300px]">
+        <Image
+          src={imageUrl || "/service/service1.jpg"}
+          alt={jaTitle || "message"}
+          layout="fill"
+          objectFit="cover"
+          className="block w-full h-full rounded-[15px]"
+        />
+      </div>
+      <div className="md:w-1/2">
+        <p className="text-accentColor text-lg font-lato font-extrabold tracking-[0.03em]">
+          {enTitle}
+        </p>
+        <h2 className="text-accentColor text-[28px] font-bold leading-[150%] tracking-[0.05em] mt-2 pb-6 border-b border-accentColor">
+          {jaTitle}
+        </h2>
+        <h3 className="text-[22px] font-medium leading-[160%] tracking-[0.03em] mt-6">
+          {convertNewLines(headingText)}
+        </h3>
+        <p className="mt-4 text-base font-normal leading-[160%] tracking-[0.02em] whitespace-pre-line">
+          {description}
+        </p>
+        {buttonHref && (
+          <MoreButton
+            href={buttonHref}
+            className="mt-6 text-accentColor border-accentColor"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ServiceSection_01;
