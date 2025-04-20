56413ba2fd2c0f38fbfaf8a3873d7501b75b9a51
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-04-20 12:55:56 +0900

    faqページ調整

--- app/(main)/faq/page.tsx
+++ app/(main)/faq/page.tsx
@@ -1,6 +1,9 @@
 import Lowerkv_01 from "@/components/lowerkv/Lowerkv_01";
-import Plan_01 from "@/components/price/plan/Plan_01";
-import Flow_01 from "@/components/top/flow/Flow_01";
+import Lowerkv_02 from "@/components/lowerkv/Lowerkv_02";
+import Lowerkv_03 from "@/components/lowerkv/Lowerkv_03";
+import Lowerkv_04 from "@/components/lowerkv/Lowerkv_04";
+import LowerFaq_01 from "@/components/faq/faq/Faq_01";
+import LowerFaq_02 from "@/components/faq/faq/Faq_02";
 import Cta_01 from "@/components/ui/module/cta/Cta_01";
 import Cta_02 from "@/components/ui/module/cta/Cta_02";
 import Cta_03 from "@/components/ui/module/cta/Cta_03";
@@ -10,8 +13,7 @@ const FaqPage = () => {
   return (
     <div>
       <Lowerkv_01 />
-      <Plan_01 />
-      <Flow_01 className="bg-bgLightBlue" />
+      <LowerFaq_01 />
       <Cta_01 />
     </div>
   );
