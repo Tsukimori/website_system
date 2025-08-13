54e9b13967ab7b45ce5c7e3e991a404208125e27
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-08-13 09:26:40 +0900

    フォントカラー設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,36 +2,20 @@ import "./globals.css"
 import type { Metadata, Viewport } from "next"
 import ToastProvider from "@/components/providers/ToastProvider"
 
-import { Noto_Sans_JP, Poppins, Lato, Spectral } from 'next/font/google'
+import { Noto_Sans_JP, Marcellus } from "next/font/google"
 
 // Noto Sans JP フォントの設定
 const notoSansJP = Noto_Sans_JP({
-  weight: ['200', '300', '400', '500','600', '700', '700','800', '900'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["200", "300", "400", "500", "600", "700", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
 })
 
-// Poppins フォントの設定
-const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-
-// Lato フォントの設定
-const lato = Lato({
-  weight: ['100', '300', '400', '700', '900'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-// Spectral フォントの設定
-const spectral = Spectral({
-  weight: ['400', '700'],  // 通常と太字を指定
-  style: ['italic'],  // 通常とイタリック体を指定
-  subsets: ['latin'],
-  display: 'swap',
+// Marcellus フォントの設定
+const marcellus = Marcellus({
+  weight: ["400"], // Marcellusは400のみ利用可能
+  subsets: ["latin"],
+  display: "swap",
 })
 
 export const metadata: Metadata = {
@@ -53,7 +37,12 @@ interface RootLayoutProps {
 const RootLayout = async ({ children }: RootLayoutProps) => {
   return (
     <html lang="ja">
-      <body className="font-notoSansJP text-baseColor">
+      <body
+        className="font-notoSansJP text-baseColor"
+        style={{
+          fontFamily: `${notoSansJP.style.fontFamily}, ${marcellus.style.fontFamily}, sans-serif`,
+        }}
+      >
         <ToastProvider />
         {children}
       </body>
