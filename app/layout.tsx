6ced24aeabb0967b3af0c9eb708cb7bd53e664f6
Tsukimori <tsukimori@day-1.tokyo>
2025-12-12 08:15:31 +0900

    更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css"
 import type { Metadata, Viewport } from "next"
 import ToastProvider from "@/components/providers/ToastProvider"
 import { Noto_Sans_JP, Lato } from "next/font/google"
-import { GoogleTagManager } from "@next/third-parties/google"
 
 // Noto Sans JP フォントの設定
 const notoSansJP = Noto_Sans_JP({
@@ -45,7 +44,7 @@ const RootLayout = async ({ children }: RootLayoutProps) => {
   return (
     <html lang="ja">
       <body className={`font-notoSansJP text-baseColor ${lato.variable}`}>
-        <GoogleTagManager gtmId="GTM-5VZQPT43" />
+  
         <ToastProvider />
         {children}
       </body>
